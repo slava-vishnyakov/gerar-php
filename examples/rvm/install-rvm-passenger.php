83a7a675ac635c5fc2f9a464466a3484bb3a7073
Slava Vishnyakov <bomboze@gmail.com>
2013-08-04 15:00:31 +0400

    RVM problems
    https://github.com/wayneeseguin/rvm/issues/2088#issuecomment-22066672

--- examples/rvm/install-rvm-passenger.php
+++ examples/rvm/install-rvm-passenger.php
@@ -17,7 +17,7 @@ class Rvm
             Package::named('curl')->shouldBeInstalled();
 
             Console::log("Installing RVM");
-            Process::runInBash('\curl -sL https://get.rvm.io | sudo bash');
+            Process::runInBash('git clone https://github.com/wayneeseguin/rvm.git; cd rvm; git checkout stable ;  ./install ; cd ..; rm -rf rvm');
         }
         return $this;
     }
